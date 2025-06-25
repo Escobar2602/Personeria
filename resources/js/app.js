3888c2e7e68d8d6de53867504ff3040c79aa68a7
Carlos Daniel Escobar Lastre <cescobarl@cotecmar.com>
2025-06-25 12:01:41 -0500

    ultimos cambios con run build y cambios a todas las vistas2

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,8 +7,7 @@ import Aura from '@primeuix/themes/aura';
 
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-
+import { Ziggy } from 'ziggy-js';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
